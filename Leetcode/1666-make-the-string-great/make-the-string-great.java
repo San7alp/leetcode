ef1742f3eee940a90e18a5692216302640fe410b
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-01 21:52:51 +0530

    Time: 7 ms (5.63%) | Memory: 43 MB (6.51%) - LeetSync

---
+++ Leetcode/1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack=new Stack<>();
+        char c[]=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty()&&Math.abs(stack.peek() - c[i]) == 32){
+                stack.pop();
+            }
+            else{
+                stack.push(c[i]);
+            }
+        }
+            String str="";
+        while (!stack.isEmpty()) {
+            char c1=stack.pop();
+            str=str+c1;
+        }
+        return new StringBuilder(str).reverse().toString();
+
+    }
+}
