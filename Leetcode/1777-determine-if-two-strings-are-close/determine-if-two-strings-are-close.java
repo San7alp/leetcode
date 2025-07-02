cc1d9b34b34704af43bda1f652ee6412e8f1803b
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-02 22:10:08 +0530

    Time: 32 ms (51.20%) | Memory: 45.5 MB (81.69%) - LeetSync

--- Leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ Leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         if(word1.length()!=word2.length()){
-            return false;
+            return false;//if words length are diff then false
         }
         // if(word1.equals(word2)){
         //     return false;
@@ -9,13 +9,13 @@ class Solution {
         HashSet<Character>set=new HashSet<>();
         for(int i=0;i<word1.length();i++){
             if(!set.contains(word1.charAt(i))){
-                set.add(word1.charAt(i));
+                set.add(word1.charAt(i));//adding each unique word to the set
             }
         }
          for(int i=0;i<word2.length();i++){
             if(!set.contains(word2.charAt(i))){
                 return false;
-            }
+            }//checking if both words have same amount of unique letters eg w1=aabbcab & w2=abcabab both have only 3 unique letters a,b,c unlike eg w1=aabbcabz & w2=abcabab w1 have a,b,c,z
         }
         int arr1[]=new int [26];
         int arr2[]=new int [26];
@@ -24,7 +24,7 @@ class Solution {
             int y=word2.charAt(i)-'a';
             arr1[x]++;
             arr2[y]++;
-         }
+         }// keeping a track of frequency of each unique letter
          for(int i=0;i<arr1.length;i++){
             for(int j=0;j<arr2.length;j++){
                 if(arr1[i]==arr2[j]){
@@ -32,7 +32,7 @@ class Solution {
                     arr2[j]=0;
                 }
             }
-         }
+         }//crossing if letter of the words have same frequency
         for(int i=0;i<arr1.length;i++){
             if(arr1[i]!=0){
                 return false;
