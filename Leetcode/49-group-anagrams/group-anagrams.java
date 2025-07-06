474405ec0a103a347d5d31f8443db915944d97e3
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-06 21:06:27 +0530

    Time: 6 ms (98.22%) | Memory: 47.9 MB (55.73%) - LeetSync

--- Leetcode/49-group-anagrams/group-anagrams.java
+++ Leetcode/49-group-anagrams/group-anagrams.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        ArrayList<List<String>> list1=new ArrayList<>();
+        
         HashMap<String,List<String>>map=new HashMap<>();
         for(int i=0;i<strs.length;i++){
             char c[]=strs[i].toCharArray();
