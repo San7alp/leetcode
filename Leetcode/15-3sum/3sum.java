ef82f4fbd70f0eb91e0edbf15a1703a00059fede
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-09 10:56:36 +0530

    Time: 31 ms (57.11%) | Memory: 52.5 MB (22.29%) - LeetSync

--- Leetcode/15-3sum/3sum.java
+++ Leetcode/15-3sum/3sum.java
@@ -2,31 +2,36 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         ArrayList<List<Integer>>res=new ArrayList<>();
-        int target=0;
         for(int i=0;i<nums.length-2;i++){
-             if( i > 0 && nums[i] == nums[i-1]){
-                continue;
+            int j=i+1;
+            int k=nums.length-1;
+            int target=-nums[i];
+            while(i<nums.length-1&&nums[i]==nums[i+1]){
+                i++;//skipping the duplicates i
             }
-            target=nums[i];
-            int j=i+1;int k=nums.length-1;
-           while(j<k){
-            if(nums[j]+nums[k]==-target){
-                ArrayList<Integer>list=new ArrayList<>();
-                list.add(nums[i]);
-                list.add(nums[j]);
-                list.add(nums[k]);
-                res.add(list);
-                while(j<k&&nums[j]==nums[j+1])j++;
-                while(j<k&&nums[k]==nums[k-1])k--;
-                k--;j++;
+            while(j<k){
+                if(nums[j]+nums[k]==target){
+                    ArrayList<Integer>list=new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    res.add(list);
+                    while(j<k&&nums[j]==nums[j+1]){
+                        j++;//skipping the duplicates j
+                    }
+                    while(j<k&&nums[k]==nums[k-1]){
+                        k--;//skipping the duplicates k
+                    }
+                    j++;
+                    k--;
+                }
+                else if(nums[j]+nums[k]<target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
             }
-            else if(nums[j]+nums[k]<-target){
-                j++;
-            }
-            else{
-                k--;
-            }
-           }
         }
         return res;
     }
