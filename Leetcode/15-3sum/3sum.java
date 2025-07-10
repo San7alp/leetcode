0da288ee581a58c4647694724ff4ffad03caf386
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-10 11:07:11 +0530

    Time: 31 ms (57.04%) | Memory: 53 MB (13.27%) - LeetSync

--- Leetcode/15-3sum/3sum.java
+++ Leetcode/15-3sum/3sum.java
@@ -1,38 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> res=new ArrayList<>();
         Arrays.sort(nums);
-        ArrayList<List<Integer>>res=new ArrayList<>();
         for(int i=0;i<nums.length-2;i++){
+            int target=-nums[i];
             int j=i+1;
             int k=nums.length-1;
-            int target=-nums[i];
-            while(i<nums.length-1&&nums[i]==nums[i+1]){
-                i++;//skipping the duplicates i
+            while(i<nums.length-1&&nums[i+1]==nums[i]){
+                i++;
             }
+            
             while(j<k){
                 if(nums[j]+nums[k]==target){
-                    ArrayList<Integer>list=new ArrayList<>();
+                    ArrayList<Integer> list=new ArrayList<>();
                     list.add(nums[i]);
-                    list.add(nums[j]);
                     list.add(nums[k]);
+                    list.add(nums[j]);
                     res.add(list);
-                    while(j<k&&nums[j]==nums[j+1]){
-                        j++;//skipping the duplicates j
-                    }
-                    while(j<k&&nums[k]==nums[k-1]){
-                        k--;//skipping the duplicates k
-                    }
-                    j++;
-                    k--;
+                    while(j<k&&nums[j]==nums[j+1]) 
+                    {j++;}
+                    while(j<k&&nums[k]==nums[k-1]) 
+                    {k--;}
+                    j++;k--;
                 }
                 else if(nums[j]+nums[k]<target){
                     j++;
                 }
-                else{
-                    k--;
-                }
+                else{k--;}
             }
         }
         return res;
+
     }
 }
