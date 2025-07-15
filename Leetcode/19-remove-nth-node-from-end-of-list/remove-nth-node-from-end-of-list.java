7beb5f7e2880b037bba3907e6f51e6e5200b38f9
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-16 00:17:27 +0530

    Time: 1 ms (3.90%) | Memory: 42.4 MB (7.96%) - LeetSync

--- Leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ Leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -4,24 +4,16 @@ class Solution {
 
         Stack<ListNode> stack = new Stack<>();
         ListNode temp = head;
-
-        // Push all nodes to the stack
         while (temp != null) {
             stack.push(temp);
             temp = temp.next;
         }
-
-        // Pop n nodes to get to the one before the node to remove
         for (int i = 0; i < n; i++) {
             temp = stack.pop();
         }
-
-        // If stack is empty, we removed the head
         if (stack.isEmpty()) {
             return head.next;
         }
-
-        // Bypass the node to be removed
         ListNode prev = stack.pop();
         prev.next = temp.next;
 
