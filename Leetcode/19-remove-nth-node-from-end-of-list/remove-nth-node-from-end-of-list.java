5e316bcde64d8f2b331e7cbbcd25e640ac1a7175
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-16 12:42:56 +0530

    Time: 1 ms (3.86%) | Memory: 42.4 MB (7.94%) - LeetSync

--- Leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ Leetcode/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -1,22 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || head.next == null) return null;
-
-        Stack<ListNode> stack = new Stack<>();
-        ListNode temp = head;
-        while (temp != null) {
+        if(head==null||head.next==null){
+            return null;
+        }
+        Stack<ListNode>stack=new Stack<>();
+        ListNode temp=head;
+        while(temp!=null){
             stack.push(temp);
-            temp = temp.next;
+            temp=temp.next;
         }
-        for (int i = 0; i < n; i++) {
-            temp = stack.pop();
+        for(int i=0;i<n;i++){
+            temp=stack.pop();
         }
-        if (stack.isEmpty()) {
-            return head.next;
+        ListNode curr;
+        if(temp==head){
+            curr=head.next;
+            temp.next=null;
+            return curr;
         }
-        ListNode prev = stack.pop();
-        prev.next = temp.next;
-
-        return head;
+        curr=stack.pop();
+        curr.next=temp.next;
+        temp.next=null;
+    return head;
     }
 }
