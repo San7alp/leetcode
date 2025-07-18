4a6634ca30d91de1505746e143561f399e36dca0
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-18 10:57:09 +0530

    Time: 32 ms (41.81%) | Memory: 52.8 MB (14.76%) - LeetSync

--- Leetcode/15-3sum/3sum.java
+++ Leetcode/15-3sum/3sum.java
@@ -1,35 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        ArrayList<List<Integer>> res=new ArrayList<>();
+        ArrayList<List<Integer>>res=new ArrayList<>();
         Arrays.sort(nums);
+        
         for(int i=0;i<nums.length-2;i++){
-            int target=-nums[i];
-            int j=i+1;
-            int k=nums.length-1;
-            while(i<nums.length-1&&nums[i+1]==nums[i]){
-                i++;
-            }
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int sum=0;
+            int left=i+1;
+            int right=nums.length-1;
             
-            while(j<k){
-                if(nums[j]+nums[k]==target){
-                    ArrayList<Integer> list=new ArrayList<>();
+            while(left<right){
+                sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    ArrayList<Integer>list=new ArrayList<>();
                     list.add(nums[i]);
-                    list.add(nums[k]);
-                    list.add(nums[j]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
                     res.add(list);
-                    while(j<k&&nums[j]==nums[j+1]) 
-                    {j++;}
-                    while(j<k&&nums[k]==nums[k-1]) 
-                    {k--;}
-                    j++;k--;
+                    while(left<right&&nums[left]==nums[left+1]){
+                        left++;
+                    }
+                    while(left<right&&nums[right]==nums[right-1]){
+                        right--;
+                    }
+                    right--;
+                    left++;
                 }
-                else if(nums[j]+nums[k]<target){
-                    j++;
+                else if(sum<0){
+                    left++;
                 }
-                else{k--;}
+                else{
+                    right--;
+                }
             }
         }
         return res;
-
     }
 }
