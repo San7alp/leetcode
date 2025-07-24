7ae9e8edf260dcb94b68c73383c64fd2ecf8512c
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-24 12:24:04 +0530

    Time: 11 ms (91.85%) | Memory: 48.7 MB (63.08%) - LeetSync

--- Leetcode/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ Leetcode/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        List<Integer>bucket[]=new List[nums.length+1];
-    
-       for(int i=0;i<nums.length;i++){
-        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-       }
-       for(int key:map.keySet()){
-        int freq=map.get(key);
-        if(bucket[freq]==null){
-            bucket[freq]=new ArrayList<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+        List<Integer>bucket[]=new ArrayList[nums.length+1];
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        bucket[freq].add(key);
-       }
-       int arr[]=new int[k];
-       int count=0;
-       for(int pos=bucket.length-1;pos>=0&&count<k;pos--){
-        if(bucket[pos]!=null){
-            for(int ele:bucket[pos]){
-                arr[count]=ele;
-                count++;
+        for(int key:map.keySet()){
+            int num=map.get(key);
+            if(bucket[num]==null){
+                bucket[num]=new ArrayList<>();
             }
+            bucket[num].add(key);
         }
-       }return arr;
-        //add number to arr check if the upcoming arr[i] has more frequncy in map than the arr
+       int arr[]=new int[k];
+        int count=0;
+        for( int pos=bucket.length-1;pos>=0&&count<k;pos--){
+            if(bucket[pos]!=null){
+                for(int ele:bucket[pos]){
+                    if(count<k){
+                        arr[count]=ele;
+                    count++;
+                    }
+                }
+            }
+
+        }
+        return arr;
     }
 }
