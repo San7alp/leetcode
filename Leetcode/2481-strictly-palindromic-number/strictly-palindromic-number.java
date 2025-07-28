375b2b2e6d1d55d8bb57ceafd62f63edade8e92c
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-07-28 13:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (24.10%) - LeetSync

---
+++ Leetcode/2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        // String binary=Integer.toBinaryString(n);
+        
+        // int left=0;
+        // int right=binary.length()-1;
+        // while(left<=right){
+        //     if(binary.charAt(left)!=binary.charAt(right)){
+        //         return false;
+        //     }
+        // }
+        return false;
+
+    }
+}
