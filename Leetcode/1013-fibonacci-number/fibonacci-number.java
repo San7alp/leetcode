cbb0c2f9319d6b147f184b1127f5f7be3b6bfdd2
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-08-04 00:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (80.72%) - LeetSync

--- Leetcode/1013-fibonacci-number/fibonacci-number.java
+++ Leetcode/1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n==1){
-            return 1;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return F(dp,n);
+        
+    }
+    public int F(int []dp,int n){
+        if(n<2){
+            return n;
         }
-        if(n==0){
-            return 0;
-        }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=F(dp,n-1)+F(dp,n-2);
     }
 }
