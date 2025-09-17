9a02e61fba497ff26f36513b3230076371753c8c
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-09-17 22:37:32 +0530

    Time: 2 ms (98.00%) | Memory: 44.6 MB (31.37%) - LeetSync

--- Leetcode/572-subtree-of-another-tree/subtree-of-another-tree.java
+++ Leetcode/572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -20,13 +20,14 @@ class Solution {
         if(check(root,subRoot)){
             return true;
         }
-        return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
-
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
     }
     public boolean check(TreeNode root,TreeNode subRoot){
         if(root==null&&subRoot==null) return true;
         if(root==null||subRoot==null) return false;
-        if(root.val!=subRoot.val) return false;
-        return check(root.left,subRoot.left)&&check(root.right,subRoot.right);
+        if(root.val!=subRoot.val){
+            return false;
+        }
+        return check(root.left,subRoot.left)&& check(root.right,subRoot.right);
     }
 }
