9cd62be19b9695c947c9b6ca85c9791547441819
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-10-21 13:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (92.91%) - LeetSync

--- Leetcode/70-climbing-stairs/climbing-stairs.java
+++ Leetcode/70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int []dp= new int[n+1];
+        int [] dp=new int[n+1];
         Arrays.fill(dp,-1);
         return solve(dp,n);
-        
     }
-     public int solve(int []dp, int n){
-        if(n<=2){
-            return n;
+    public int solve(int []dp,int n){
+        if(n<2){
+            return 1;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
         return dp[n]=solve(dp,n-1)+solve(dp,n-2);
-     }
+    }
 }
