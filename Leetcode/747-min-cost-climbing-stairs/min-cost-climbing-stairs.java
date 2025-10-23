fea3abac325be572f70cb285e41dd8c39291b79d
Sankalp Mohapatra <sankalpmohappatra2003@gmail.com>
2025-10-23 21:32:53 +0530

    Time: 1 ms (36.46%) | Memory: 43 MB (90.43%) - LeetSync

--- Leetcode/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ Leetcode/747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,14 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int []dp=new int[cost.length+1];
+        int n=cost.length;
+         int []dp=new int[n+1];
         Arrays.fill(dp,-1);
-        int s1=solve(dp,cost,0);
-        int s2=solve(dp,cost,1);
-        return Math.min(s1,s2);
+        // return Math.min(cost[0],cost[1])+solve(cost,dp,n,2);
+        int n1=solve(cost,dp,n,0);
+        int n2=solve(cost,dp,n,1);
+        return Math.min(n1,n2);
     }
-    public int solve(int[]dp,int[]nums,int i){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=Math.min(nums[i]+solve(dp,nums,i+1),nums[i]+solve(dp,nums,i+2));
+    public int solve(int[]nums,int []dp,int n,int i){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i]=Math.min(nums[i]+solve(nums,dp,n,i+1),nums[i]+solve(nums,dp,n,i+2));
     }
 }
